2c3614e317003fb1f29617f52e3506e87b21eafa
zys8119 <770959294@qq.com>
2021-10-20 18:12:28 +0800

    [add] 项目搭建

--- src/view/System/Layout/LayoutHeader.vue
+++ src/view/System/Layout/LayoutHeader.vue
@@ -1,15 +1,34 @@
 <template>
-<div class="LayoutHeader">
-    头部
-</div>
+    <div class="LayoutHeader">
+        <div class="Logo">
+            <span>阅</span>
+            无纸化会议系统
+        </div>
+        <div class="LayoutHeaderContent">
+            <div class="LayoutHeaderContentItem">asdas</div>
+        </div>
+    </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
     name: "LayoutHeader"
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.LayoutHeader{
+    display: flex;
+    justify-items: center;
+    align-items: center;
+    .logo{
 
+    }
+    .LayoutHeaderContent{
+        flex: 1;
+        display: flex;
+        justify-items: center;
+        align-items: center;
+    }
+}
 </style>
