9cbeecd1b8728011f7874e37fd0326f67ec3964d
zys8119 <770959294@qq.com>
2021-11-18 10:57:52 +0800

    [add] 初始化项目

--- src/view/Test/Alert/SizeTest.vue
+++ src/view/Test/Alert/SizeTest.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="SizeTest">
-        <div class="aa">字体大小测试</div>
+        <div class="aa">移动端字体大小测试</div>
         <div>asdaasdasd5545646464646499999</div>
         {{css__rem__base}}
         <el-button class="aa" @click="$emit('aaa',4556)">{{ airforce.aaa }}</el-button>
@@ -23,7 +23,7 @@ const css__rem__base = ref(477)
 <style scoped lang="less">
 .SizeTest {
     .aa{
-        font-size: size(60);
+        font-size: size(14);
     }
 }
 </style>
