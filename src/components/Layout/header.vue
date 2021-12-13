35215ca9d25bc802b5b3ac69f7733048872a9ef8
chentao <jooies@qq.com>
2021-12-13 12:02:31 +0800

    [mod] 优化样式

--- src/components/Layout/header.vue
+++ src/components/Layout/header.vue
@@ -1,16 +1,13 @@
 <template>
     <div class="main-header-left">
         <el-button v-if="!store.isH5" :icon="Expand" :class="{ expand: store.collapse }" @click="store.collapse = !store.collapse" />
-        <el-breadcrumb separator="/">
+        <el-breadcrumb v-if="!store.isH5" separator="/">
             <el-breadcrumb-item v-for="route in routeMatched" :key="route.name" :to="{ path: route.path }">{{ route.meta.title || route.name }}</el-breadcrumb-item>
         </el-breadcrumb>
     </div>
     <el-dropdown>
         <span class="el-dropdown-link">
             {{ store.name }}
-            <el-icon class="el-icon--right">
-                <arrow-down />
-            </el-icon>
         </span>
         <template #dropdown>
             <el-dropdown-menu>
@@ -20,7 +17,7 @@
             </el-dropdown-menu>
         </template>
     </el-dropdown>
-    <el-button size="small" @click="handleLogout">注销</el-button>
+    <el-button size="small" type="text" class="logout-button" @click="handleLogout">注销</el-button>
 </template>
 
 <script lang="ts" setup>
@@ -41,3 +38,9 @@ const routeMatched = computed(() => {
     return route.matched.slice(1)
 })
 </script>
+
+<style lang="less" scoped>
+.logout-button {
+    margin-left: 10px;
+}
+</style>
