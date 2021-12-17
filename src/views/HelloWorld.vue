c704298ae8b61b95748850dd1dc4ccba312bba29
chentao <jooies@qq.com>
2021-12-17 14:49:35 +0800

    [mod] 删除多余的代码

--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>
-      Hello, World.
+        Hello, World.
     </h1>
 </template>
 
@@ -9,21 +9,3 @@ export default defineComponent({
     name: 'Home'
 })
 </script>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
