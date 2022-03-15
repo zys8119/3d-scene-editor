5a85a134506141b10700f0dd57ef85a1a23cd158
chentao <jooies@qq.com>
2022-03-15 09:23:06 +0800

    [mod] better permission

--- src/utils/usePermission.ts
+++ src/utils/usePermission.ts
@@ -2,6 +2,7 @@ import { RouteLocationNormalizedLoaded } from 'vue-router'
 
 export const hasPermissions = (
     route: RouteLocationNormalizedLoaded,
+    method: 'some' | 'every',
     ...args: (string | string[])[]
 ): boolean => {
     if (
@@ -9,24 +10,15 @@ export const hasPermissions = (
         route.meta.permissions.length === 0
     ) return false
     const flatArgs = args.flat()
-    return route.meta.permissions.some(permission => flatArgs.includes(permission))
-}
-
-export const hasAllPermissions = (route: RouteLocationNormalizedLoaded, ...args: (string | string[])[]): boolean => {
-    if (
-        !route.meta.permissions ||
-        route.meta.permissions.length === 0
-    ) return false
-    const flatArgs = args.flat()
-    return route.meta.permissions.every(permission => flatArgs.includes(permission))
+    return route.meta.permissions[method](permission => flatArgs.includes(permission))
 }
 
 export const usePermission = () => {
     const route = useRoute()
     return {
         route,
-        has: (...args: (string | string[])[]) => hasPermissions(route, ...args),
-        hasAll: (...args: (string | string[])[]) => hasAllPermissions(route, ...args),
+        has: (...args: (string | string[])[]) => hasPermissions(route, 'some', ...args),
+        hasAll: (...args: (string | string[])[]) => hasPermissions(route, 'every', ...args),
         permissions: route.meta.permissions || []
     }
 }
