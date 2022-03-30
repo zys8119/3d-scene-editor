23e33a9d5ca7e3e985ee8ab59be3adbd2d12434a
chentao <jooies@qq.com>
2022-03-30 17:20:43 +0800

    [mod] new 404 page

--- src/components/Common/404.vue
+++ src/components/Common/404.vue
@@ -1,9 +1,16 @@
 <template>
-    <wp-result description="啥也没有" />
+    <wp-result message="404 NOT FOUND" description="你要的并不在这里">
+        <template #suffix>
+            <wp-button size="small" @click="$router.push('/')">返回首页</wp-button>
+        </template>
+    </wp-result>
 </template>
 
 <style lang="less" scoped>
 .wp-result {
     margin-top: 20vh;
+}
+.wp-button {
+    margin-top: 20px;
 }
 </style>
