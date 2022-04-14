4dc6742322f3393a521ae68317cfe89cb5ef45b7
chentao <jooies@qq.com>
2022-04-14 11:54:10 +0800

    [mod] add ref

--- src/utils/vite/rem.ts
+++ src/utils/vite/rem.ts
@@ -1,9 +1,35 @@
+let baseOption = 1920
+
+export const fz = (rem: number) => {
+    return `${Number(rem) / baseOption * 100}vw`
+}
+
+export const remReplace = (string: string) => {
+    return string.replace(
+        /([0-9.]{1,}?)fz/img,
+        (match, $1) => {
+            return fz($1)
+        }
+    )
+}
+
 export const rem = ({
     base
 }: { base: number } = { base: 1920 }) => {
+    baseOption = base || 1920
     return {
         name: 'rem',
         transform(code: string, id: string, ssr?: boolean) {
+            if (/\.vue$/.test(id)) {
+                code = code.replace(/(\{ style: {(.*)?} \})/ig, (match, $1: string) => {
+                    return remReplace($1)
+                })
+                return {
+                    code,
+                    map: null
+                }
+            }
+
             if (!/\.scss|\.css|\.less$/.test(id)) {
                 return {
                     code,
@@ -11,12 +37,7 @@ export const rem = ({
                 }
             }
 
-            code = code.replace(
-                /size\(([0-9.]{1,}?)\)/img,
-                (match, $1) => {
-                    return `calc(${Number($1) / base} * 100vw)`
-                }
-            )
+            code = remReplace(code)
 
             return {
                 code,
--- src/views/Demo/Test2.vue
+++ src/views/Demo/Test2.vue
@@ -1,4 +1,5 @@
 <template>
+    <span :style="`font-size: ${a}`">232323</span>
     <div id="Test2">
         <wp-pro-permission has="admin">
             权限测试，这里只有 admin 权限才可以看
@@ -14,12 +15,16 @@
 </template>
 
 <script lang="ts" setup>
+import { fz } from '@/utils/vite/rem'
 import { usePermission } from 'wisdom-plus'
 const { has } = usePermission()
+
+const a = ref(fz(20))
+console.log(a.value)
 </script>
 
 <style scoped>
 #Test2 {
-    font-size: size(100);
+    font-size: 100fz;
 }
 </style>
