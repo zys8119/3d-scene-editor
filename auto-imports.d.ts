3c3759fc759c8a1845d4fc637b0d671f299c4c01
xuyi <598491063>
2022-04-14 10:29:35 +0800

    [mod] update wp-request version

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -235,6 +235,5 @@ declare global {
   const watchOnce: typeof import('@vueuse/core')['watchOnce']
   const watchWithFilter: typeof import('@vueuse/core')['watchWithFilter']
   const whenever: typeof import('@vueuse/core')['whenever']
-  const WpToast: typeof import('wisdom-plus/es')['WpToast']
 }
 export {}
