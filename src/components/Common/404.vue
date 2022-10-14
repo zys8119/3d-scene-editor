49c5c7412ddd4296bc0f199ebaefe35abe89bcc9
chentao <jooies@qq.com>
2022-10-14 13:48:52 +0800

    [mod] add error message

--- src/components/Common/404.vue
+++ src/components/Common/404.vue
@@ -1,10 +1,18 @@
 <template>
-    <wp-result message="404 NOT FOUND" description="你要的并不在这里">
+    <wp-result :message="error ?? '404 NOT FOUND'" description="你要的并不在这里">
         <template #suffix>
-            <wp-button size="small" @click="$router.push('/')">返回首页</wp-button>
+            <wp-button size="small" @click="refresh">刷新</wp-button>
         </template>
     </wp-result>
 </template>
+
+<script lang="ts" setup>
+defineProps<{
+    error?: string
+}>()
+
+const refresh = () => location.reload()
+</script>
 
 <style lang="less" scoped>
 .wp-result {
