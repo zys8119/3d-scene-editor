77ff5c113c1a468b5ca15c12d7f803a21adbc6d1
chentao <jooies@qq.com>
2022-11-18 17:07:30 +0800

    [chore] update vue-tsc

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -10,7 +10,6 @@ declare global {
   const ElNotification: typeof import('element-plus/es')['ElNotification']
   const RequestPromise: typeof import('wp-request')['RequestPromise']
   const ValidationError: typeof import('wp-validate')['ValidationError']
-  const WpInput: typeof import('wisdom-plus/es')['WpInput']
   const acceptHMRUpdate: typeof import('pinia')['acceptHMRUpdate']
   const api: typeof import('@/api')['api']
   const asyncComputed: typeof import('@vueuse/core')['asyncComputed']
