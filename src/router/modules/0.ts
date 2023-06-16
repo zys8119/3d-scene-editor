dcf478aafbd4cd4c294443ea74022bd4013074ee
zys8119 <770959294@qq.com>
2023-06-16 22:03:46 +0800

    [add] deps

--- src/router/modules/0.ts
+++
@@ -1,8 +0,0 @@
-import { RouteRecordRaw } from 'vue-router';
-
-const routes: RouteRecordRaw[] = [
-    {
-        path: 'h5',
-        component: () => import('@/components/layout/main-h5'),
-    },
-];
