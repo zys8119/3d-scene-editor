6996c9fe4826650cbeaa50e2f5376d4bb1922e51
xy <598491063@qq.com>
2023-06-20 15:18:44 +0800

    [mod] add loading

--- src/components/common/loading.vue
+++ src/components/common/loading.vue
@@ -1,5 +1,7 @@
 <template>
-    <n-spin description="加载中" :size="36" />
+    <n-space class="w-100vw h-100vh" justify="center" align="center">
+        <n-spin description="页面初始化中..." :size="36" />
+    </n-space>
 </template>
 
 <style lang="less" scoped>
